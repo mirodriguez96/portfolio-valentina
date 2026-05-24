4ac1358ebcc615fbf2aba1b00ab39a026ad8fa3f
Miguel Rodriguez <mirodriguezor@unal.edu.co>
2026-05-24 14:28:06 -0500

    Configuracion para despliegue a produccion

--- webpersonal/wsgi.py
+++ webpersonal/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for webpersonal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# DESPLEGAR APP EN PYTHONANYWHERE
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpersonal.settings')
+# Raiz donde va el proyecto y se encuentra el archivo manage.py
+path = os.path.expanduser("~/portfolio-valentina")
+if path not in sys.path:
+    sys.path.insert(0, path)
 
-application = get_wsgi_application()
+# nombre del proyecto
+os.environ["DJANGO_SETTINGS_MODULE"] = "webpersonal.settings"
+
+
+application = StaticFilesHandler(get_wsgi_application())
