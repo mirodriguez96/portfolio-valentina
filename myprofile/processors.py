bd9eb6aa35bb4169eeb60a96d60fc98ff6ed63b3
Miguel Rodriguez <mirodriguezor@unal.edu.co>
2026-05-24 14:41:47 -0500

    Ajuste en diccionario

--- myprofile/processors.py
+++ myprofile/processors.py
@@ -4,16 +4,24 @@ from .models import Profile
 def ctx_dict(request):
     ctx = {}
     profiles = Profile.objects.all()
+    ctx = {
+        "my_name": "Mi nombre",
+        "my_occupation": "Mi ocupación",
+        "my_avatar": "",
+        "my_biography": "Mi biografía",
+        "my_age": "Mi edad",
+        "my_phone": "Mi teléfono",
+        "my_email": "Mi correo electrónico",
+    }
+
     for profile in profiles:
         ctx = {
-            "my_name": profile.name if profile.name else "Mi nombre",
-            "my_occupation": (
-                profile.occupation if profile.occupation else "Mi ocupación"
-            ),
+            "my_name": profile.name,
+            "my_occupation": (profile.occupation),
             "my_avatar": profile.my_avatar,
-            "my_biography": profile.biography if profile.biography else "Mi biografía",
-            "my_age": profile.age if profile.age else "Mi edad",
-            "my_phone": profile.phone if profile.phone else "Mi teléfono",
-            "my_email": profile.email if profile.email else "Mi correo electrónico",
+            "my_biography": profile.biography,
+            "my_age": profile.age,
+            "my_phone": profile.phone,
+            "my_email": profile.email,
         }
     return ctx
